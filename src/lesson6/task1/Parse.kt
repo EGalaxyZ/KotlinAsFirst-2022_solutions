b985960a9ca4661a0bbaf60f9be6d2df9212794b
Egor <egor_zybin@mail.ru>
2022-11-30 15:21:06 +0300

    Исправление

--- src/lesson6/task1/Parse.kt
+++ src/lesson6/task1/Parse.kt
@@ -115,31 +115,25 @@ fun dateStrToDigit(str: String): String {
  * входными данными.
  */
 fun dateDigitToStr(digital: String): String {
-    val line = digital.split(".").toMutableList()
-    try {
-        if ((line[0].toInt() !in 0..daysInMonth(line[1].toInt(), line[2].toInt()) || line.size > 3)) return ""
-        line[1] = when (line[1]) {
-            "01" -> "января"
-            "02" -> "февраля"
-            "03" -> "марта"
-            "04" -> "апреля"
-            "05" -> "мая"
-            "06" -> "июня"
-            "07" -> "июля"
-            "08" -> "августа"
-            "09" -> "сентября"
-            "10" -> "октября"
-            "11" -> "ноября"
-            "12" -> "декабря"
-            else -> "-1"
-        }
-        return if (line[1] == "-1") ""
-        else String.format("%d %s %d", line[0].toInt(), line[1], line[2].toInt())
-    } catch (I: IndexOutOfBoundsException) {
-        return ""
-    } catch (E: NumberFormatException) {
-        return ""
+    val neededString = Regex("""([0123][123456789])\.([01][123456789])\.([012]\d{3})""")
+    if (!neededString.matches(digital)) return ""
+    val dateList = neededString.find(digital)!!.groupValues.toMutableList()
+    if (dateList[1].toInt() !in 0..daysInMonth(dateList[2].toInt(), dateList[3].toInt())) return ""
+    dateList[2] = when (dateList[2]) {
+        "01" -> "января"
+        "02" -> "февраля"
+        "03" -> "марта"
+        "04" -> "апреля"
+        "05" -> "мая"
+        "06" -> "июня"
+        "07" -> "июля"
+        "08" -> "августа"
+        "09" -> "сентября"
+        "10" -> "октября"
+        "11" -> "ноября"
+        else -> "декабря"
     }
+    return String.format("%d %s %s", dateList[1].toInt(), dateList[2], dateList[3])
 }
 
 /**
